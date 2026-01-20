21d582af30a414fed75040555e3183eb9e8cb51d
TIN Developer <developer@tin.info>
2026-01-20 16:29:33 +0100

    Remove client subdirectory (unused React app)

--- client/src/containers/Home/RecallCalendar/Loaders/Ping.tsx
+++
@@ -1,8 +0,0 @@
-export default function Ping() {
-  return (
-    <span className="relative flex w-4 h-4">
-      <span className="absolute inline-flex w-full h-full bg-indigo-400 rounded-full opacity-75 animate-ping"></span>
-      <span className="relative inline-flex w-4 h-4 bg-indigo-500 rounded-full"></span>
-    </span>
-  );
-}
--- client/src/containers/Home/RecallCalendar/Loaders/index.tsx
+++
@@ -1,9 +0,0 @@
-import Ping from "./Ping";
-import Spinner from "./Spinner";
-
-const LoaderFactory = {
-  Spinner,
-  Ping,
-};
-
-export default LoaderFactory;
